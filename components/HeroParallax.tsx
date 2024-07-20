b14da0b50188668ebec85b0e6a1cf78b7a703484
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-07-20 13:27:54 -0600

    start

---
+++ components/HeroParallax.tsx
@@ -0,0 +1,87 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
+
+export function HeroParallaxDemo() {
+	return <HeroParallax products={products} />;
+}
+export const products = [
+	{
+		title: "Moonbeam",
+		link: "https://gomoonbeam.com",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Cursor",
+		link: "https://cursor.so",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Rogue",
+		link: "https://userogue.com",
+		thumbnail: "/room1.JPG",
+	},
+
+	{
+		title: "Editorially",
+		link: "https://editorially.org",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Editrix AI",
+		link: "https://editrix.ai",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Pixel Perfect",
+		link: "https://app.pixelperfect.quest",
+		thumbnail: "/room1.JPG",
+	},
+
+	{
+		title: "Algochurn",
+		link: "https://algochurn.com",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Aceternity UI",
+		link: "https://ui.aceternity.com",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Tailwind Master Kit",
+		link: "https://tailwindmasterkit.com",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "SmartBridge",
+		link: "https://smartbridgetech.com",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Renderwork Studio",
+		link: "https://renderwork.studio",
+		thumbnail: "/room1.JPG",
+	},
+
+	{
+		title: "Creme Digital",
+		link: "https://cremedigital.com",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Golden Bells Academy",
+		link: "https://goldenbellsacademy.com",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "Invoker Labs",
+		link: "https://invoker.lol",
+		thumbnail: "/room1.JPG",
+	},
+	{
+		title: "E Free Invoice",
+		link: "https://efreeinvoice.com",
+		thumbnail: "/room1.JPG",
+	},
+];
