b2adb150e40b04e7f025bdcfee043de26426f92a
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-07-25 17:06:56 -0600

    schedule

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -6,6 +6,41 @@ const Schedule = () => {
 			<h1 className="text-[2rem] chakra mx-auto flex items-center justify-center pb-[1rem] mb-4 border-b-[1px] border-neutral-800 w-full">
 				Schedule
 			</h1>
+			{/* 
+            <div className="flex flex-col">
+				<h1 className="text-[1.75rem] text-neutral-200 chakra mx-auto flex items-center justify-center pb-[0.5rem] w-full">
+					AGES GUIDE
+				</h1>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">6:30 am - 7:30 am</div>
+					<div className="">Adults</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">9:00 - 10:00</div>
+					<div className="">Family</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">4:00 - 5:00</div>
+					<div className="">Adults</div>
+				</div>
+			</div> */}
+			<div className="flex flex-col">
+				<h1 className="text-[1.75rem] text-neutral-200 chakra mx-auto flex items-center justify-center pb-[0.5rem] w-full">
+					Ages Quide
+				</h1>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">3-8</div>
+					<div className="">Kids</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">9:00 - 10:00</div>
+					<div className="">Family</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">4:00 - 5:00</div>
+					<div className="">Adults</div>
+				</div>
+			</div>
 			<div className="flex flex-col">
 				<h1 className="text-[1.75rem] text-neutral-200 chakra mx-auto flex items-center justify-center pb-[0.5rem] w-full">
 					Monday
@@ -23,9 +58,60 @@ const Schedule = () => {
 					<div className="">Adults</div>
 				</div>
 			</div>
-			<div className="flex flex-col">
+			<div className="flex flex-col mt-4">
 				<h1 className="text-[1.75rem] text-neutral-200 chakra mx-auto flex items-center justify-center pb-[0.5rem] w-full">
 					Tuesday
+				</h1>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">6:30 am - 7:30 am</div>
+					<div className="">Adults</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">9:00 - 10:00</div>
+					<div className="">Family</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">4:00 - 5:00</div>
+					<div className="">Adults</div>
+				</div>
+			</div>
+			<div className="flex flex-col mt-4">
+				<h1 className="text-[1.75rem] text-neutral-200 chakra mx-auto flex items-center justify-center pb-[0.5rem] w-full">
+					Wednesday
+				</h1>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">6:30 am - 7:30 am</div>
+					<div className="">Adults</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">9:00 - 10:00</div>
+					<div className="">Family</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">4:00 - 5:00</div>
+					<div className="">Adults</div>
+				</div>
+			</div>
+			<div className="flex flex-col mt-4">
+				<h1 className="text-[1.75rem] text-neutral-200 chakra mx-auto flex items-center justify-center pb-[0.5rem] w-full">
+					Thursday
+				</h1>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">6:30 am - 7:30 am</div>
+					<div className="">Adults</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">9:00 - 10:00</div>
+					<div className="">Family</div>
+				</div>
+				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
+					<div className="chakra">4:00 - 5:00</div>
+					<div className="">Adults</div>
+				</div>
+			</div>
+			<div className="flex flex-col mt-4">
+				<h1 className="text-[1.75rem] text-neutral-200 chakra mx-auto flex items-center justify-center pb-[0.5rem] w-full">
+					Thursday
 				</h1>
 				<div className="flex items-center justify-between px-4 border-b-[1px] border-neutral-800 pb-3 pt-3 text-[1.15rem]">
 					<div className="chakra">6:30 am - 7:30 am</div>
